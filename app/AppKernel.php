00e51a1a910a4d65ef6c59eb480db91ab2c6e9c6
Philipp Wahala <philipp.wahala@gmail.com>
2013-09-02 14:21:53 +0200

    Basic setup of EcommerceCoreBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
+            // new Liip\DoctrineCacheBundle\LiipDoctrineCacheBundle(),
             new Sandbox\MainBundle\SandboxMainBundle(),
+            new Ecommerce\Bundle\CoreBundle\EcommerceCoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
